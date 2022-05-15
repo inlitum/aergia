eb21bb4377c8d2b5ab5120e0071a9a50086de6d8
Jack Borrie <lunarinlitum@hotmail.com>
2022-05-15 17:11:34 +1200

    Testing deployment routes

--- start/routes.ts
+++ start/routes.ts
@@ -1,38 +1,20 @@
 import Route from '@ioc:Adonis/Core/Route';
 
+Route.get('/api/accounts', 'AccountsController.index').middleware('auth:api');
 
-Route.group (() => {
-    // Accounts
-    Route.group (() => {
-        Route.get ('accounts', 'AccountsController.index');
-        Route.get ('account/:id', 'AccountsController.show');
-        Route.post ('account', 'AccountsController.create');
-        Route.put ('account/:id', 'AccountsController.update')
-        Route.delete ('account/:id', 'AccountsController.destroy')
-    });
-    // Transactions
-    Route.group (() => {
-        Route.get ('transactions', 'TransactionsController.index');
-        Route.get ('transaction/:id', 'TransactionsController.show');
-        Route.post ('transaction', 'TransactionsController.create');
-        Route.put ('transaction/:id', 'TransactionsController.update');
-        Route.delete ('transaction/:id', 'TransactionsController.destroy');
-    }).prefix ('account/:account_id');
+Route.get       ('accounts', 'AccountsController.index').middleware('auth:api');
+Route.get       ('account/:id', 'AccountsController.show').middleware('auth:api');
+Route.post      ('account', 'AccountsController.create').middleware('auth:api');
+Route.put       ('account/:id', 'AccountsController.update').middleware('auth:api');
+Route.delete    ('account/:id', 'AccountsController.destroy').middleware('auth:api');
+Route.get       ('transactions', 'TransactionsController.index').middleware('auth:api');
+Route.get       ('transaction/:id', 'TransactionsController.show').middleware('auth:api');
+Route.post      ('transaction', 'TransactionsController.create').middleware('auth:api');
+Route.put       ('transaction/:id', 'TransactionsController.update').middleware('auth:api');
+Route.delete    ('transaction/:id', 'TransactionsController.destroy').middleware('auth:api');
 
-    Route.get ('transactions', 'TransactionsController.index');
-
-    Route.group (() => {
-        Route.get ('ingredient/:id', 'IngredientsController.show');
-    });
-
-    Route.get ('ingredients', 'IngredientsController.index');
-
-}).middleware ('auth:api').prefix ('api');
-
-Route.group (() => {
-    Route.post ('login', 'AuthController.login');
-    Route.post ('register', 'AuthController.register');
-}).prefix('auth');
+Route.post      ('login', 'AuthController.login');
+Route.post      ('register', 'AuthController.register');
 
 Route.get('/', async ({view}) => {
     return await view.render('welcome', {});
