219336a5c57f1a181c13dfa4b6412068036d819b
Jack Borrie <lunarinlitum@hotmail.com>
2022-05-19 20:18:16 +1200

    Testing deployment routes

--- start/routes.ts
+++ start/routes.ts
@@ -1,44 +1,39 @@
-import Route   from '@ioc:Adonis/Core/Route';
-import Logger  from '@ioc:Adonis/Core/Logger'
+import Route from '@ioc:Adonis/Core/Route';
 
-// Route.get('/api/accounts', 'AccountsController.index').middleware('auth:api');
-//
-// Route.get       ('accounts', 'AccountsController.index').middleware('auth:api');
-// Route.get       ('account/:id', 'AccountsController.show').middleware('auth:api');
-// Route.post      ('account', 'AccountsController.create').middleware('auth:api');
-// Route.put       ('account/:id', 'AccountsController.update').middleware('auth:api');
-// Route.delete    ('account/:id', 'AccountsController.destroy').middleware('auth:api');
-// Route.get       ('transactions', 'TransactionsController.index').middleware('auth:api');
-// Route.get       ('transaction/:id', 'TransactionsController.show').middleware('auth:api');
-// Route.post      ('transaction', 'TransactionsController.create').middleware('auth:api');
-// Route.put       ('transaction/:id', 'TransactionsController.update').middleware('auth:api');
-// Route.delete    ('transaction/:id', 'TransactionsController.destroy').middleware('auth:api');
-//
-// Route.post      ('login', 'AuthController.login');
-// Route.post      ('register', 'AuthController.register');
-// //
 
-Route.group(() => {
-    Route.get('test', ({request}) => {
-        Logger.info(request.method());
-        return {
-            status: 200,
-            data: {
-                message: 'Get test',
-            },
-        };
+Route.group (() => {
+    // Accounts
+    Route.group (() => {
+        Route.get ('accounts', 'AccountsController.index');
+        Route.get ('account/:id', 'AccountsController.show');
+        Route.post ('account', 'AccountsController.create');
+        Route.put ('account/:id', 'AccountsController.update')
+        Route.delete ('account/:id', 'AccountsController.destroy')
     });
-    Route.post('test', ({request}) => {
-        Logger.info(request.method());
-        return {
-            status: 200,
-            data: {
-                message: 'Post test',
-            },
-        };
-    });
-});
+    // Transactions
+    Route.group (() => {
+        Route.get ('transactions', 'TransactionsController.index');
+        Route.get ('transaction/:id', 'TransactionsController.show');
+        Route.post ('transaction', 'TransactionsController.create');
+        Route.put ('transaction/:id', 'TransactionsController.update');
+        Route.delete ('transaction/:id', 'TransactionsController.destroy');
+    }).prefix ('account/:account_id');
 
-Route.get('', () => {
-    return 'Hello World';
-});
+    Route.get ('transactions', 'TransactionsController.index');
+
+    Route.group (() => {
+        Route.get ('ingredient/:id', 'IngredientsController.show');
+    });
+
+    Route.get ('ingredients', 'IngredientsController.index');
+
+}).middleware ('auth:api').prefix ('api');
+
+Route.group (() => {
+    Route.post ('login', 'AuthController.login');
+    Route.post ('register', 'AuthController.register');
+}).prefix('auth');
+
+Route.get('/', async ({view}) => {
+    return await view.render('welcome', {});
+})
