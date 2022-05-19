da552adbebb5760b90e4f37f902b508b71783b92
Jack Borrie <lunarinlitum@hotmail.com>
2022-05-19 20:32:52 +1200

    Testing deployment routes

--- start/routes.ts
+++ start/routes.ts
@@ -32,7 +32,9 @@ Route.group (() => {
 Route.group (() => {
     Route.post ('login', 'AuthController.login');
     Route.post ('register', 'AuthController.register');
-}).prefix('auth');
+})
+
+
 
 Route.get('/', async ({view}) => {
     return await view.render('welcome', {});
