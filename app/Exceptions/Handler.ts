4ab47c1356e1e70791f8ce7b9e1d2ee36ec92b3f
Jack Borrie <lunarinlitum@hotmail.com>
2022-05-20 10:24:56 +1200

    Updating deploy script to include database migration.

--- app/Exceptions/Handler.ts
+++ app/Exceptions/Handler.ts
@@ -13,26 +13,26 @@
 |
 */
 
-import Logger from '@ioc:Adonis/Core/Logger'
-import HttpExceptionHandler from '@ioc:Adonis/Core/HttpExceptionHandler'
+import Logger                  from '@ioc:Adonis/Core/Logger'
+import HttpExceptionHandler    from '@ioc:Adonis/Core/HttpExceptionHandler'
 import { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
 
 export default class ExceptionHandler extends HttpExceptionHandler {
-  protected statusPages = {
-    '403': 'errors/unauthorized',
-    '404': 'errors/not-found',
-    '500..599': 'errors/server-error',
-  }
-
-  constructor() {
-    super(Logger)
-  }
-
-  public async handle(error: any, ctx: HttpContextContract) {
-    if (error.code === 'E_UNAUTHORIZED_ACCESS') {
-      return ctx.response.status(401).send(error.message)
+    protected statusPages = {
+        '403':      'errors/unauthorized',
+        '404':      'errors/not-found',
+        '500..599': 'errors/server-error',
     }
 
-    return super.handle(error, ctx)
-  }
+    constructor () {
+        super (Logger)
+    }
+
+    public async handle (error: any, ctx: HttpContextContract) {
+        if (error.code === 'E_UNAUTHORIZED_ACCESS') {
+            return ctx.response.status (401).send (error.message)
+        }
+
+        return error.code;
+    }
 }
