87c554c88fed2a095f1bfe03a97de92272a4df27
Jack Borrie <lunarinlitum@hotmail.com>
2022-05-23 16:26:57 +1200

    Updating node

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 echo "=+=+=+=+=+=+=+= Closing existing aergia instances =+=+=+=+=+=+=+="
 pm2 stop aergia
 
@@ -25,8 +26,12 @@ echo "=+=+=+=+=+=+=+= Copying env file =+=+=+=+=+=+=+="
 cp ../../.env ./.env
 
 echo "=+=+=+=+=+=+=+= Migrating database =+=+=+=+=+=+=+="
-node ace migration:run
-node ace db:seed
+if [ "$1" = "db" ]; then
+  node ace migration:run
+  node ace db:seed
+else
+    echo "Strings are not equal."
+fi
 
 echo "=+=+=+=+=+=+=+= Starting Aergia =+=+=+=+=+=+=+="
 pm2 start server.js --name aergia
