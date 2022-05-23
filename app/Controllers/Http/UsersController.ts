811f3371ed96d7f407f1c7383be63d53a2b8a1a3
Jack Borrie <lunarinlitum@hotmail.com>
2022-05-23 15:04:30 +1200

    Checking why user groups isn't loading correctly

--- app/Controllers/Http/UsersController.ts
+++ app/Controllers/Http/UsersController.ts
@@ -50,7 +50,7 @@ export default class UsersController {
 
         Logger.info (`User-Index: Attempting to get user [${ requestUserId }] using account [${ currentUserId }]`);
         if (hasGroup (currentUser.userGroups, ['admin_read', 'admin_write']) && currentUserId !== requestUserId) {
-            Logger.info ('Has user group?', hasGroup (currentUser.userGroups, ['admin_read', 'admin_write']));
+            Logger.info ('Has user group?' + hasGroup (currentUser.userGroups, ['admin_read', 'admin_write']));
             return response.unauthorized ();
         }
 
